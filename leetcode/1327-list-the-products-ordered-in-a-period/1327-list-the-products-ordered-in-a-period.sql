8fc43f180a9cac38286bb495fe17485726c6c032
SunKim <sunhyuk22k@gmail.com>
2024-12-28 20:46:11 +0900

    Time: 679 ms (89.2%), Space: 0B (100%) - LeetHub

---
+++ leetcode/1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT
+    P.product_name,
+    SUM(unit) AS unit
+FROM
+    Products AS P
+LEFT JOIN
+    Orders AS O
+ON
+    P.product_id = O.product_id
+WHERE
+    O.order_date LIKE ('%2020-02%')
+GROUP BY
+    P.product_name
+HAVING
+    unit >= 100;
