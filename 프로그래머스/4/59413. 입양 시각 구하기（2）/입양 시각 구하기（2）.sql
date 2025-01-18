b95226ec4539777804159397aeded42d05292bae
SunKim <sunhyuk22k@gmail.com>
2025-01-18 13:55:26 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,34 @@
+/*
+  1. 시간대별 입양 건수
+  2. 입양이 없는 건수도 0으로 출력 (RECURSIVE CTE)
+*/
+-- 코드를 입력하세요
+WITH RECURSIVE HOURS AS (
+    SELECT 
+        0 AS HOUR 
+    UNION ALL
+    SELECT 
+        HOUR + 1
+    FROM 
+        HOURS
+    WHERE 
+        HOUR < 23
+)
+SELECT
+    H.HOUR,
+    COALESCE(COUNT(A.ANIMAL_ID), 0) AS COUNT
+FROM
+    HOURS AS H
+LEFT JOIN
+    (
+        SELECT
+            HOUR(DATETIME) AS HOUR,
+            ANIMAL_ID
+        FROM
+            ANIMAL_OUTS
+    ) AS A
+    ON H.HOUR = A.HOUR
+GROUP BY
+    H.HOUR
+ORDER BY
+    H.HOUR ASC;
