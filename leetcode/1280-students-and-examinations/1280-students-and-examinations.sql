f4adbcc6d1af1513244182db627cf55f1bbebf77
SunKim <sunhyuk22k@gmail.com>
2025-02-26 00:20:46 +0900

    Time: 2371 ms (12.57%), Space: 0B (100%) - LeetHub

--- leetcode/1280-students-and-examinations/1280-students-and-examinations.sql
+++ leetcode/1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,22 +1,43 @@
 # Write your MySQL query statement below
+-- SELECT
+--     s.student_id,
+--     s.student_name,
+--     sub.subject_name,
+--     COUNT(e.subject_name) AS attended_exams
+-- FROM
+--     Students AS s
+-- CROSS JOIN
+--     Subjects AS sub
+-- LEFT JOIN
+--     Examinations AS e
+-- ON
+--     s.student_id = e.student_id
+--     AND sub.subject_name = e.subject_name
+-- GROUP BY
+--     s.student_id,
+--     s.student_name,
+--     sub.subject_name
+-- ORDER BY
+--     s.student_id ASC,
+--     sub.subject_name ASC;
+
+
 SELECT
-    s.student_id,
-    s.student_name,
-    sub.subject_name,
-    COUNT(e.subject_name) AS attended_exams
+    S.student_id,
+    S.student_name,
+    SS.subject_name,
+    COUNT(E.STUDENT_ID) AS attended_exams
 FROM
-    Students AS s
+    STUDENTS S
 CROSS JOIN
-    Subjects AS sub
+    SUBJECTS SS
 LEFT JOIN
-    Examinations AS e
-ON
-    s.student_id = e.student_id
-    AND sub.subject_name = e.subject_name
+    EXAMINATIONS AS E
+    ON S.STUDENT_ID = E.STUDENT_ID
+    AND SS.SUBJECT_NAME = E.SUBJECT_NAME
 GROUP BY
-    s.student_id,
-    s.student_name,
-    sub.subject_name
+    S.student_id,
+    S.student_name,
+    SS.subject_name
 ORDER BY
-    s.student_id ASC,
-    sub.subject_name ASC;
+    1 ASC;
