8d00a76d1aa3da2593890620502a329af1386eb7
SunKim <sunhyuk22k@gmail.com>
2025-03-01 19:41:17 +0900

    Time: 1744 ms (26.54%), Space: 0B (100%) - LeetHub

--- leetcode/1251-average-selling-price/1251-average-selling-price.sql
+++ leetcode/1251-average-selling-price/1251-average-selling-price.sql
@@ -1,16 +1,41 @@
 # Write your MySQL query statement below
+-- SELECT
+--     p.product_id,
+--     (CASE
+--         WHEN SUM(COALESCE(us.units,0)) = 0 THEN 0 
+--         ELSE ROUND(SUM(p.price * us.units) / SUM(us.units),2) 
+--     END) AS average_price
+-- FROM
+--     Prices AS p
+-- LEFT JOIN
+--     UnitsSold AS us
+-- ON
+--     p.product_id = us.product_id
+--     AND (us.purchase_date BETWEEN p.start_date AND p.end_date)
+-- GROUP BY
+--     p.product_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 SELECT
     p.product_id,
-    (CASE
-        WHEN us.units = 0 THEN 0 
-        ELSE ROUND(SUM(p.price * us.units) / SUM(us.units),2) 
-    END) AS average_price
+    IFNULL(ROUND(SUM(U.units * P.price) / SUM(U.units), 2),0) AS average_price
 FROM
-    Prices AS p
+    PRICES AS P
 LEFT JOIN
-    UnitsSold AS us
-ON
-    p.product_id = us.product_id
-    AND (us.purchase_date BETWEEN p.start_date AND p.end_date)
+    UNITSSOLD AS U
+    ON P.PRODUCT_ID = U.PRODUCT_ID
+    AND (U.PURCHASE_DATE BETWEEN P.START_DATE AND P.END_DATE)
 GROUP BY
-    p.product_id;
+    P.product_id
