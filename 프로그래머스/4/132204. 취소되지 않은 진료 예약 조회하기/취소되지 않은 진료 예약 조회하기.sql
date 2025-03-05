daf881136fa7ed0e9822eb8d78e02a32fc55cd0a
SunKim <sunhyuk22k@gmail.com>
2025-03-05 17:30:12 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -7,24 +7,51 @@
  3. 정렬 : 진료예약일시
 */
 -- 코드를 입력하세요
+# SELECT
+#     AP.APNT_NO,
+#     P.PT_NAME,
+#     P.PT_NO,
+#     AP.MCDP_CD,
+#     D.DR_NAME,
+#     AP.APNT_YMD
+# FROM
+#     APPOINTMENT AS AP
+# INNER JOIN
+#     PATIENT AS P
+#     ON AP.PT_NO = P.PT_NO
+# INNER JOIN
+#     DOCTOR AS D
+#     ON AP.MDDR_ID = D.DR_ID
+# WHERE
+#     DATE_FORMAT(AP.APNT_YMD,'%Y-%m-%d') = '2022-04-13'
+#     AND AP.MCDP_CD = 'CS'
+#     AND AP.APNT_CNCL_YN = 'N'
+# ORDER BY
+#     AP.APNT_YMD ASC;
+
+
+/*
+ - 2022.04.13일에 취소되지 않은 흉부외과(CS) 진료 예약 내역 조회
+*/
+
 SELECT
-    AP.APNT_NO,
+    A.APNT_NO,
     P.PT_NAME,
-    P.PT_NO,
-    AP.MCDP_CD,
+    A.PT_NO,
+    A.MCDP_CD,
     D.DR_NAME,
-    AP.APNT_YMD
+    A.APNT_YMD
 FROM
-    APPOINTMENT AS AP
-INNER JOIN
-    PATIENT AS P
-    ON AP.PT_NO = P.PT_NO
+    APPOINTMENT AS A
 INNER JOIN
     DOCTOR AS D
-    ON AP.MDDR_ID = D.DR_ID
+    ON A.MDDR_ID = D.DR_ID
+INNER JOIN
+    PATIENT AS P
+    ON A.PT_NO = P.PT_NO
 WHERE
-    DATE_FORMAT(AP.APNT_YMD,'%Y-%m-%d') = '2022-04-13'
-    AND AP.MCDP_CD = 'CS'
-    AND AP.APNT_CNCL_YN = 'N'
+    A.MCDP_CD LIKE ('CS')
+    AND DATE_FORMAT(A.APNT_YMD,'%Y-%m-%d') = '2022-04-13'
+    AND A.APNT_CNCL_YN NOT IN ('Y')
 ORDER BY
-    AP.APNT_YMD ASC;
+    A.APNT_YMD ASC;
