bb22881e2ebdc00e354e616ba838f47a6dfba315
SunKim <sunhyuk22k@gmail.com>
2025-03-07 11:57:36 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -4,33 +4,64 @@
  3. 정렬 : 판매일, 상품ID, 유저ID 오름차순
 */
 -- 코드를 입력하세요
+# SELECT
+#     SALES_DATE,
+#     PRODUCT_ID,
+#     USER_ID,
+#     SALES_AMOUNT
+# FROM 
+#     (
+#         SELECT
+#             DATE_FORMAT(SALES_DATE,'%Y-%m-%d') AS SALES_DATE,
+#             PRODUCT_ID,
+#             USER_ID,
+#             SALES_AMOUNT
+#         FROM 
+#             ONLINE_SALE
+#         WHERE
+#             SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31'
+#         UNION ALL
+#         SELECT
+#             SALES_DATE,
+#             PRODUCT_ID,
+#             NULL AS USER_ID,
+#             SALES_AMOUNT
+#         FROM
+#             OFFLINE_SALE
+#         WHERE
+#             SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31'
+#     ) AS UNION_TABLE
+# ORDER BY
+#     SALES_DATE ASC,
+#     PRODUCT_ID ASC,
+#     USER_ID ASC;
+
+
+/*
+ - 2022-03월 오프라인/온라인 상품 판매 데이터 출력
+ - offline_sale 테이블 user_id 값은 null로 표기
+ - 판매일,상품ID,유저ID 기준 오름차순 정렬
+*/
+
 SELECT
-    SALES_DATE,
+    DATE_FORMAT(SALES_DATE,'%Y-%m-%d') AS SALES_DATE,
     PRODUCT_ID,
     USER_ID,
     SALES_AMOUNT
-FROM 
-    (
-        SELECT
-            DATE_FORMAT(SALES_DATE,'%Y-%m-%d') AS SALES_DATE,
-            PRODUCT_ID,
-            USER_ID,
-            SALES_AMOUNT
-        FROM 
-            ONLINE_SALE
-        WHERE
-            SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31'
-        UNION ALL
-        SELECT
-            SALES_DATE,
-            PRODUCT_ID,
-            NULL AS USER_ID,
-            SALES_AMOUNT
-        FROM
-            OFFLINE_SALE
-        WHERE
-            SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31'
-    ) AS UNION_TABLE
+FROM
+    ONLINE_SALE
+WHERE
+    SALES_DATE LIKE ('2022-03%')
+UNION ALL
+SELECT
+    DATE_FORMAT(SALES_DATE,'%Y-%m-%d') AS SALES_DATE,
+    PRODUCT_ID,
+    NULL AS USER_ID,
+    SALES_AMOUNT
+FROM
+    OFFLINE_SALE
+WHERE
+    SALES_DATE LIKE ('2022-03%')
 ORDER BY
     SALES_DATE ASC,
     PRODUCT_ID ASC,
