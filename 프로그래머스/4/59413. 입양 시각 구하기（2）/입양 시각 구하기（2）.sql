010fdf00baa705a7bc6a06c45a574ac47c0d314b
SunKim <sunhyuk22k@gmail.com>
2025-03-08 12:06:54 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -3,32 +3,65 @@
   2. 입양이 없는 건수도 0으로 출력 (RECURSIVE CTE)
 */
 -- 코드를 입력하세요
-WITH RECURSIVE HOURS AS (
-    SELECT 
-        0 AS HOUR 
+# WITH RECURSIVE HOURS AS (
+#     SELECT 
+#         0 AS HOUR 
+#     UNION ALL
+#     SELECT 
+#         HOUR + 1
+#     FROM 
+#         HOURS
+#     WHERE 
+#         HOUR < 23
+# )
+# SELECT
+#     H.HOUR,
+#     COALESCE(COUNT(A.ANIMAL_ID), 0) AS COUNT
+# FROM
+#     HOURS AS H
+# LEFT JOIN
+#     (
+#         SELECT
+#             HOUR(DATETIME) AS HOUR,
+#             ANIMAL_ID
+#         FROM
+#             ANIMAL_OUTS
+#     ) AS A
+#     ON H.HOUR = A.HOUR
+# GROUP BY
+#     H.HOUR
+# ORDER BY
+#     H.HOUR ASC;
+
+
+WITH RECURSIVE CHECK_HOURS AS (
+    SELECT
+        0 AS HOUR
     UNION ALL
-    SELECT 
+    SELECT
         HOUR + 1
-    FROM 
-        HOURS
-    WHERE 
+    FROM
+        CHECK_HOURS
+    WHERE
         HOUR < 23
 )
-SELECT
-    H.HOUR,
-    COALESCE(COUNT(A.ANIMAL_ID), 0) AS COUNT
+
+SELECT 
+    CH.HOUR,
+    IF(AO.COUNT <> 0, AO.COUNT, 0) AS COUNT
 FROM
-    HOURS AS H
+    CHECK_HOURS AS CH
 LEFT JOIN
     (
         SELECT
             HOUR(DATETIME) AS HOUR,
-            ANIMAL_ID
-        FROM
+            COUNT(*) AS COUNT
+        FROM    
             ANIMAL_OUTS
-    ) AS A
-    ON H.HOUR = A.HOUR
-GROUP BY
-    H.HOUR
-ORDER BY
-    H.HOUR ASC;
+        GROUP BY
+            HOUR(DATETIME)            
+    ) AS AO
+    ON CH.HOUR = AO.HOUR
+
+
+ 
