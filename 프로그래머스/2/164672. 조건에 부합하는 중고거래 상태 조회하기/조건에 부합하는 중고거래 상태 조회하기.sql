a593983a8aea793bdc037b4e5233d427d7a88728
SunKim <sunhyuk22k@gmail.com>
2025-03-20 11:26:30 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,14 +1,32 @@
 -- 코드를 입력하세요
-SELECT  BOARD_ID
-        ,WRITER_ID
-        ,TITLE
-        ,PRICE
-        ,CASE
-            WHEN STATUS = 'DONE' THEN '거래완료'
-            WHEN STATUS = 'SALE' THEN '판매중'
-            WHEN STATUS = 'RESERVED' THEN '예약중'
-        END STATUS
-  FROM  USED_GOODS_BOARD
- WHERE  CREATED_DATE IN ('2022-10-05%')
- ORDER
-    BY  BOARD_ID DESC;
+# SELECT  BOARD_ID
+#         ,WRITER_ID
+#         ,TITLE
+#         ,PRICE
+#         ,CASE
+#             WHEN STATUS = 'DONE' THEN '거래완료'
+#             WHEN STATUS = 'SALE' THEN '판매중'
+#             WHEN STATUS = 'RESERVED' THEN '예약중'
+#         END STATUS
+#   FROM  USED_GOODS_BOARD
+#  WHERE  CREATED_DATE IN ('2022-10-05%')
+#  ORDER
+#     BY  BOARD_ID DESC;
+
+
+SELECT
+    BOARD_ID,
+    WRITER_ID,
+    TITLE,
+    PRICE,
+    CASE
+        WHEN STATUS = 'DONE' THEN '거래완료'
+        WHEN STATUS = 'SALE' THEN '판매중'
+        ELSE '예약중'
+    END
+FROM
+    USED_GOODS_BOARD
+WHERE
+    CREATED_DATE LIKE ('2022-10-05')
+ORDER BY
+    1 DESC;
