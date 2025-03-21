5ed350abc5619b83906ee7e2f42690ae834431eb
SunKim <sunhyuk22k@gmail.com>
2025-03-21 12:17:45 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,8 +1,11 @@
 -- 코드를 입력하세요
-SELECT  ANIMAL_ID
-        ,NAME
-        ,SEX_UPON_INTAKE
-  FROM  ANIMAL_INS
- WHERE  NAME IN ('LUCY', 'ELLA', 'PICKLE', 'ROGAN', 'SABRINA', 'MITTY')
- ORDER
-    BY  ANIMAL_ID ASC;
+SELECT
+    ANIMAL_ID,
+    NAME,
+    SEX_UPON_INTAKE
+FROM
+    ANIMAL_INS
+WHERE
+    NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY
+    1 ASC;
