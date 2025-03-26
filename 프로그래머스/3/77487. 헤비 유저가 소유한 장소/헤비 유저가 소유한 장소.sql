7529766d9ebf4bafa8fbcec59cf1b8fda1113785
SunKim <sunhyuk22k@gmail.com>
2025-03-26 12:21:11 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,14 +1,38 @@
 -- 코드를 입력하세요
+# SELECT
+#     *
+# FROM
+#     PLACES
+# WHERE
+#     HOST_ID IN (
+#         SELECT HOST_ID
+#         FROM PLACES
+#         GROUP BY HOST_ID
+#         HAVING COUNT(HOST_ID) >= 2
+#     )
+# ORDER BY
+#     ID ASC;
+
+
+WITH CHECK_HEAVY_HOST AS 
+(
+    SELECT
+        HOST_ID
+    FROM
+        PLACES
+    GROUP BY
+        HOST_ID
+    HAVING
+        COUNT(*) >= 2
+)
+
 SELECT
-    *
+    ID,
+    NAME,
+    HOST_ID
 FROM
     PLACES
 WHERE
-    HOST_ID IN (
-        SELECT HOST_ID
-        FROM PLACES
-        GROUP BY HOST_ID
-        HAVING COUNT(HOST_ID) >= 2
-    )
+    HOST_ID IN (SELECT HOST_ID FROM CHECK_HEAVY_HOST)
 ORDER BY
     ID ASC;
