55badd36abf1171996b8c352d693bb766177473f
SunKim <sunhyuk22k@gmail.com>
2025-03-27 18:26:08 +0900

    [level 3] Title: 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/README.md
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/README.md
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 10일 00:46:30
+2025년 03월 27일 18:26:06
 
 ### 문제 설명
 
@@ -119,9 +119,12 @@
 <th>FISH_TYPE</th>
 </tr>
 </thead>
-        <tbody></tbody>
+        <tbody><tr>
+<td>3</td>
+<td>50</td>
+<td>0</td>
+</tr>
+</tbody>
       </table>
-<p>| 3 | 50 | 0 |</p>
-
 
 > 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -1,20 +1,55 @@
 -- 코드를 작성해주세요
+# SELECT
+#     COUNT(*) AS FISH_COUNT,
+#     MAX(LENGTH) AS MAX_LENGTH,
+#     FISH_TYPE
+# FROM
+#     (
+#         SELECT ID, FISH_TYPE, TIME,
+#             (CASE 
+#                 WHEN LENGTH IS NULL THEN 10
+#                 ELSE LENGTH
+#              END) AS LENGTH
+#         FROM FISH_INFO
+#     ) AS FISH_INFO
+# GROUP BY
+#     FISH_TYPE
+# HAVING
+#     AVG(LENGTH) >= 33
+# ORDER BY
+#     FISH_TYPE;
+
+/*
+평균길이 33cm 이상 물고기 출력
+*/
+WITH FISH_LIST AS (
+    SELECT
+        FISH_TYPE,
+        CASE
+            WHEN LENGTH IS NULL THEN 10
+            ELSE LENGTH
+        END LENGTH
+    FROM
+        FISH_INFO
+), FISH_FIND AS (
+SELECT
+    FISH_TYPE   
+FROM
+    FISH_LIST
+GROUP BY
+    FISH_TYPE
+HAVING
+    AVG(LENGTH) >= 33
+)
 SELECT
     COUNT(*) AS FISH_COUNT,
     MAX(LENGTH) AS MAX_LENGTH,
     FISH_TYPE
 FROM
-    (
-        SELECT ID, FISH_TYPE, TIME,
-            (CASE 
-                WHEN LENGTH IS NULL THEN 10
-                ELSE LENGTH
-             END) AS LENGTH
-        FROM FISH_INFO
-    ) AS FISH_INFO
+    FISH_LIST
+WHERE
+    FISH_TYPE IN (SELECT FISH_TYPE FROM FISH_FIND)
 GROUP BY
     FISH_TYPE
-HAVING
-    AVG(LENGTH) >= 33
 ORDER BY
-    FISH_TYPE;
+    FISH_TYPE ASC;
