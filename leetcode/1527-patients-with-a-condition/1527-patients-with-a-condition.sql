fd19d1e01ee9e0f7987e76622fb30dac7b287849
SunKim <sunhyuk22k@gmail.com>
2025-04-13 12:07:08 +0900

    Time: 433 ms (67.46%), Space: 0B (100%) - LeetHub

--- leetcode/1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ leetcode/1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,10 +1,20 @@
 # Write your MySQL query statement below
+-- SELECT
+--     patient_id,
+--     patient_name,
+--     conditions
+-- FROM
+--     Patients
+-- WHERE
+--     conditions LIKE ('DIAB1%')
+--     OR conditions LIKE ('% DIAB1%');
+
 SELECT
     patient_id,
     patient_name,
     conditions
 FROM
-    Patients
+    PATIENTS
 WHERE
-    conditions LIKE ('DIAB1%')
-    OR conditions LIKE ('% DIAB1%');
+    CONDITIONS LIKE ('DIAB1%')
+    OR CONDITIONS LIKE ('% DIAB1%');
