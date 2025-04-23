3ff2203e385f782e30b2ede0a815f7024a297158
SunKim <sunhyuk22k@gmail.com>
2025-04-24 02:52:23 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -1,8 +1,26 @@
 -- 코드를 작성해주세요
-SELECT  ID
-        ,LENGTH
-  FROM  FISH_INFO
- ORDER
-    BY  LENGTH DESC
-        ,ID ASC
- LIMIT  10;
+# SELECT  ID
+#         ,LENGTH
+#   FROM  FISH_INFO
+#  ORDER
+#     BY  LENGTH DESC
+#         ,ID ASC
+#  LIMIT  10;
+
+WITH RN_FISH AS (
+    SELECT
+        ID,
+        LENGTH,
+        RANK() OVER(ORDER BY LENGTH DESC) AS RN
+    FROM
+        FISH_INFO
+)
+SELECT
+    ID,
+    LENGTH
+FROM
+    RN_FISH
+WHERE
+    RN <= 10
+# ORDER BY
+#     1 ASC;
