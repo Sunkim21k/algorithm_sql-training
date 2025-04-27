8cf1075fd62bd7b6c75ffe799d10bc8f73936260
SunKim <sunhyuk22k@gmail.com>
2025-04-27 18:10:59 +0900

    Time: 664 ms (77.87%), Space: 0B (100%) - LeetHub

--- leetcode/1934-confirmation-rate/1934-confirmation-rate.sql
+++ leetcode/1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,11 +1,22 @@
 # Write your MySQL query statement below
+-- SELECT
+--     S.USER_ID,
+--     ROUND(SUM(IF(C.ACTION = 'confirmed',1,0)) / IFNULL(count(*),1),2) AS confirmation_rate
+-- FROM
+--     SIGNUPS AS S
+-- LEFT JOIN
+--     CONFIRMATIONS AS C
+--     ON S.USER_ID = C.USER_ID
+-- GROUP BY
+--     S.USER_ID;
+
 SELECT
-    S.USER_ID,
-    ROUND(SUM(IF(C.ACTION = 'confirmed',1,0)) / IFNULL(count(*),1),2) AS confirmation_rate
+    S.user_id,
+    ROUND(SUM(IF(C.action = 'confirmed',1,0)) / IFNULL(COUNT(*),1),2) AS confirmation_rate
 FROM
     SIGNUPS AS S
 LEFT JOIN
     CONFIRMATIONS AS C
     ON S.USER_ID = C.USER_ID
 GROUP BY
-    S.USER_ID;
+    S.USER_ID
