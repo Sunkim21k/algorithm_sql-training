e611fa6339dab7bb264631a3c70d28d225b70e5f
SunKim <sunhyuk22k@gmail.com>
2025-04-29 00:38:31 +0900

    Time: 351 ms (75.9%), Space: 0B (100%) - LeetHub

--- leetcode/0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ leetcode/0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,16 +1,36 @@
 # Write your MySQL query statement below
+-- SELECT
+--     name
+-- FROM
+--     EMPLOYEE
+-- WHERE
+--     id IN (
+--         SELECT
+--             managerId
+--         FROM
+--             Employee
+--         GROUP BY
+--             managerId
+--         HAVING
+--             COUNT(managerId) >= 5
+--     );
+
+WITH CHECK_MANAGER AS (
+    SELECT
+        managerId,
+        COUNT(*) AS cnt
+    FROM
+        EMPLOYEE
+    WHERE
+        managerId IS NOT NULL
+    GROUP BY
+        managerId
+    HAVING
+        cnt >= 5
+)
 SELECT
     name
 FROM
     EMPLOYEE
 WHERE
-    ID IN (
-SELECT
-    MANAGERID
-FROM
-    EMPLOYEE
-GROUP BY
-    MANAGERID
-HAVING
-    COUNT(MANAGERID) >= 5
-    );
+    id IN (SELECT managerId FROM CHECK_MANAGER);
