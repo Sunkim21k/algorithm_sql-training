a82f89188c92869997272facdb69fae13384aa31
SunKim <sunhyuk22k@gmail.com>
2025-05-06 12:37:54 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- leetcode/1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ leetcode/1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,23 +1,47 @@
 # Write your MySQL query statement below
-WITH user_delivery_list AS (
+-- WITH user_delivery_list AS (
+--     SELECT
+--         *,
+--         CASE
+--             WHEN order_date = customer_pref_delivery_date THEN 'immediate'
+--             ELSE 'scheduled'
+--         END AS is_check,
+--         ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date ASC) AS rn
+--     FROM
+--         DELIVERY
+-- )
+-- SELECT
+--     ROUND((SUM(IF(is_check = 'immediate',1,0)) / COUNT(*))*100,2) AS immediate_percentage
+-- FROM
+--     (    
+--         SELECT 
+--             is_check
+--         FROM
+--             user_delivery_list
+--         WHERE
+--             rn = 1
+--     ) as D
+
+WITH CHECK_ORDER_LIST AS (
     SELECT
-        *,
-        CASE
-            WHEN order_date = customer_pref_delivery_date THEN 'immediate'
-            ELSE 'scheduled'
-        END AS is_check,
-        ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date ASC) AS rn
+        ROW_NUMBER() OVER(PARTITION BY CUSTOMER_ID ORDER BY ORDER_DATE DESC) AS RN,
+        CUSTOMER_ID,
+        CASE 
+            WHEN ORDER_DATE = CUSTOMER_PREF_DELIVERY_DATE THEN 'IMMEDIATE'
+            ELSE 'SCHEDULED'
+        END AS CHECK_ORDER
     FROM
         DELIVERY
+), CHECK_IM AS (
+    SELECT
+        COUNT(*) AS CNT
+    FROM
+        CHECK_ORDER_LIST
+    WHERE
+        RN = 1
+        AND CHECK_ORDER LIKE 'IMMEDIATE'
 )
 SELECT
-    ROUND((SUM(IF(is_check = 'immediate',1,0)) / COUNT(*))*100,2) AS immediate_percentage
+    ROUND((SELECT CNT FROM CHECK_IM) / COUNT(DISTINCT CUSTOMER_ID) * 100,2) AS immediate_percentage
 FROM
-    (    
-        SELECT 
-            is_check
-        FROM
-            user_delivery_list
-        WHERE
-            rn = 1
-    ) as D
+    DELIVERY
