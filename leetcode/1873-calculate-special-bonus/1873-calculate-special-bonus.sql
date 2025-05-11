c38d11e4518e069ebec199fffde35ca575d0aa2d
SunKim <sunhyuk22k@gmail.com>
2025-05-11 13:20:09 +0900

    Time: 610 ms (63.2%), Space: 0B (100%) - LeetHub

--- leetcode/1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ leetcode/1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,11 +1,23 @@
 # Write your MySQL query statement below
+-- SELECT
+--     employee_id,
+--     (CASE
+--         WHEN (employee_id % 2 = 0) OR LEFT(name,1) = 'M' THEN 0
+--         ELSE salary
+--     END) AS bonus
+-- FROM
+--     Employees
+-- ORDER BY
+--     employee_id ASC;
+
+
 SELECT
     employee_id,
-    (CASE
-        WHEN (employee_id % 2 = 0) OR LEFT(name,1) = 'M' THEN 0
+    CASE
+        WHEN LEFT(NAME, 1) = 'M' OR (employee_id % 2) = 0 THEN 0
         ELSE salary
-    END) AS bonus
+    END AS bonus
 FROM
-    Employees
+    EMPLOYEES
 ORDER BY
-    employee_id ASC;
+    1 ASC;
