d3a8e599f10e44f8e17833df4566ea55a072b2c3
SunKim <sunhyuk22k@gmail.com>
2025-05-12 00:27:29 +0900

    Time: 1185 ms (84.78%), Space: 0B (100%) - LeetHub

--- leetcode/1158-market-analysis-i/1158-market-analysis-i.sql
+++ leetcode/1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,15 +1,28 @@
 # Write your MySQL query statement below
+-- SELECT
+--     U.user_id AS buyer_id,
+--     U.join_date,
+--     SUM(IF(YEAR(O.ORDER_DATE) = 2019, 1, 0)) AS orders_in_2019
+-- FROM
+--     USERS AS U
+-- LEFT JOIN
+--     ORDERS AS O
+--     ON O.BUYER_ID = U.USER_ID
+-- GROUP BY
+--     U.user_id,
+--     U.JOIN_DATE
+-- ORDER BY
+--     U.user_id;
+
 SELECT
-    U.user_id AS buyer_id,
-    U.join_date,
-    SUM(IF(YEAR(O.ORDER_DATE) = 2019, 1, 0)) AS orders_in_2019
+    user_id AS buyer_id,
+    join_date,
+    SUM(IF(YEAR(order_date) = 2019, 1, 0)) AS orders_in_2019
 FROM
     USERS AS U
 LEFT JOIN
-    ORDERS AS O
-    ON O.BUYER_ID = U.USER_ID
+    ORDERS AS O ON U.USER_ID = O.BUYER_ID
 GROUP BY
-    U.user_id,
-    U.JOIN_DATE
+    user_id
 ORDER BY
-    U.user_id;
+    1 ASC;
