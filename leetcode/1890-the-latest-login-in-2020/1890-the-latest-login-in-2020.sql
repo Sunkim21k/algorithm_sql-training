b3584032676ce478e5222eb4017f7846aa714f2f
SunKim <sunhyuk22k@gmail.com>
2025-05-13 00:17:25 +0900

    Time: 645 ms (65.85%), Space: 0B (100%) - LeetHub

--- leetcode/1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ leetcode/1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,10 +1,20 @@
 # Write your MySQL query statement below
+-- SELECT
+--     user_id,
+--     MAX(time_stamp) AS last_stamp
+-- FROM
+--     Logins
+-- WHERE
+--     YEAR(time_stamp) = 2020
+-- GROUP BY
+--     user_id;
+
 SELECT
     user_id,
     MAX(time_stamp) AS last_stamp
 FROM
-    Logins
+    LOGINS
 WHERE
     YEAR(time_stamp) = 2020
 GROUP BY
-    user_id;
+    user_id
