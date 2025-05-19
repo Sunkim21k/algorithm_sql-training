c165511cbe0a5deb1b6f6ae8564bf875a42bad25
SunKim <sunhyuk22k@gmail.com>
2025-05-20 00:07:36 +0900

    [level 2] Title: 동물 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59406. 동물 수 구하기/동물 수 구하기.sql
+++ 프로그래머스/2/59406. 동물 수 구하기/동물 수 구하기.sql
@@ -1,5 +1,10 @@
 -- 코드를 입력하세요
-SELECT  
-    COUNT(*) AS count
-FROM  
-    ANIMAL_INS;
+# SELECT  
+#     COUNT(*) AS count
+# FROM  
+#     ANIMAL_INS;
+
+SELECT
+    COUNT(ANIMAL_ID) AS count
+FROM
+    ANIMAL_INS
