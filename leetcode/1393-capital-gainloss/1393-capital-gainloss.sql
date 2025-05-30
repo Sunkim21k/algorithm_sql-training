51c7db0a8b1ee437eb30f60bc9b3bb23edf05caa
SunKim <sunhyuk22k@gmail.com>
2025-05-30 21:38:39 +0900

    Time: 473 ms (94.28%), Space: 0B (100%) - LeetHub

--- leetcode/1393-capital-gainloss/1393-capital-gainloss.sql
+++ leetcode/1393-capital-gainloss/1393-capital-gainloss.sql
@@ -1,8 +1,18 @@
 # Write your MySQL query statement below
 SELECT
     stock_name,
-    SUM(IF(OPERATION = 'SELL',PRICE,0)) - SUM(IF(OPERATION = 'BUY',PRICE,0)) AS capital_gain_loss
+    SUM(price) AS capital_gain_loss
 FROM
-    STOCKS
+    (
+        SELECT
+            stock_name,
+            operation_day,
+            CASE
+                WHEN operation LIKE 'Buy' THEN -price
+                ELSE price
+            END price
+        FROM
+            stocks
+    ) AS t1
 GROUP BY
-    STOCK_NAME;
+    stock_name
