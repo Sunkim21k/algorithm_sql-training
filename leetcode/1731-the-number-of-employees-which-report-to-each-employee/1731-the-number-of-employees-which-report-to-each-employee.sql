6effabaf7f4ec7e5bcab71c18f9b3e78c6c79fbb
SunKim <sunhyuk22k@gmail.com>
2025-06-03 00:53:32 +0900

    Time: 693 ms (33.8%), Space: 0B (100%) - LeetHub

--- leetcode/1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ leetcode/1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,45 +1,17 @@
 # Write your MySQL query statement below
--- SELECT
---     E2.employee_id,
---     E2.name,
---     COUNT(E1.reports_to) AS reports_count,
---     ROUND(AVG(E1.age)) AS average_age
--- FROM
---     Employees AS E1
--- LEFT JOIN
---     Employees AS E2
--- ON
---     E1.reports_to = E2.employee_id
--- WHERE
---     E2.employee_id IS NOT NULL
--- GROUP BY
---     E2.employee_id,
---     E2.name
--- ORDER BY
---     E2.employee_id ASC;
-
-WITH FIND_NAME AS (
-    SELECT
-        reports_to,
-        COUNT(*) AS reports_count,
-        ROUND(AVG(age)) AS average_age
-    FROM
-        EMPLOYEES
-    WHERE
-        reports_to IS NOT NULL
-    GROUP BY
-        reports_to
-)
-
 SELECT
-    employee_id,
-    name,
-    reports_count,
-    average_age
+    E2.employee_id,
+    E2.name,
+    COUNT(*) AS reports_count,
+    ROUND(AVG(E1.age),0) AS average_age
 FROM
-    EMPLOYEES AS E
-INNER JOIN
-    FIND_NAME AS F
-    ON E.EMPLOYEE_ID = F.REPORTS_TO
+    EMPLOYEES AS E1
+LEFT JOIN
+    EMPLOYEES AS E2
+    ON E1.reports_to = E2.employee_id
+WHERE
+    E1.reports_to IS NOT NULL
+GROUP BY
+    E2.employee_id
 ORDER BY
     1 ASC;
