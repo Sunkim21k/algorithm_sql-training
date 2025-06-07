ce50fcca02db35b0ef41aa762c480aea14eb1d8f
SunKim <sunhyuk22k@gmail.com>
2025-06-08 01:49:12 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,8 +1,19 @@
 -- 코드를 입력하세요
-SELECT  ANIMAL_ID
-        ,NAME
-  FROM  ANIMAL_INS
- WHERE  NAME LIKE '%EL%'
-   AND  ANIMAL_TYPE = 'Dog'
- ORDER
-    BY  NAME ASC;
+# SELECT  ANIMAL_ID
+#         ,NAME
+#   FROM  ANIMAL_INS
+#  WHERE  NAME LIKE '%EL%'
+#    AND  ANIMAL_TYPE = 'Dog'
+#  ORDER
+#     BY  NAME ASC;
+
+SELECT
+    ANIMAL_ID,
+    NAME
+FROM
+    ANIMAL_INS
+WHERE
+    ANIMAL_TYPE LIKE 'Dog'
+    AND NAME LIKE '%el%'
+ORDER BY
+    2 ASC;
