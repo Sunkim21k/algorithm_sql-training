160dff033bffc510ba6fd34158f92e4a3c2a70d8
SunKim <sunhyuk22k@gmail.com>
2025-07-06 00:08:40 +0900

    Add solution for Fix Names in a Table

---
+++ LeetCode/Fix-Names-in-a-Table/Solution.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION uppercase_first_letter(val VARCHAR(40))
+  RETURNS VARCHAR(40) DETERMINISTIC
+  RETURN CONCAT(UPPER(SUBSTR(val, 1, 1)), LOWER(SUBSTR(val, 2)));
+
+CREATE VIEW solution AS
+  SELECT user_id, uppercase_first_letter(name) AS name
+  FROM Users
+  ORDER BY name;
+
+SELECT * FROM solution;
