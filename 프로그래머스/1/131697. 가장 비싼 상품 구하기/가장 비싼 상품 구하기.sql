f5dec697a3d7dc250b37e53a290c22e3e93e0e5c
SunKim <sunhyuk22k@gmail.com>
2025-07-19 00:00:45 +0900

    [level 1] Title: 가장 비싼 상품 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131697. 가장 비싼 상품 구하기/가장 비싼 상품 구하기.sql
+++ 프로그래머스/1/131697. 가장 비싼 상품 구하기/가장 비싼 상품 구하기.sql
@@ -2,15 +2,20 @@
 # SELECT  MAX(PRICE) AS MAX_PRICE
 #   FROM  PRODUCT;
 
+# SELECT
+#     PRICE AS MAX_PRICE
+# FROM
+#     (
+#         SELECT
+#             ROW_NUMBER() OVER(ORDER BY PRICE DESC) AS RN,
+#             PRICE
+#         FROM
+#             PRODUCT
+#     ) F1
+# WHERE
+#     RN = 1
+
 SELECT
-    PRICE AS MAX_PRICE
+    MAX(PRICE) AS MAX_PRICE
 FROM
-    (
-        SELECT
-            ROW_NUMBER() OVER(ORDER BY PRICE DESC) AS RN,
-            PRICE
-        FROM
-            PRODUCT
-    ) F1
-WHERE
-    RN = 1
+    PRODUCT
