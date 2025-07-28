f079ea9d76014c726568e1d6024d8eedaf8cbb41
SunKim <sunhyuk22k@gmail.com>
2025-07-29 00:08:26 +0900

    [level 1] Title: 인기있는 아이스크림, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/133024. 인기있는 아이스크림/인기있는 아이스크림.sql
+++ 프로그래머스/1/133024. 인기있는 아이스크림/인기있는 아이스크림.sql
@@ -5,17 +5,23 @@
 #     BY  TOTAL_ORDER DESC
 #         ,SHIPMENT_ID ASC;
 
-SELECT
-    *
-FROM
-    (
-        SELECT
-            FLAVOR
-        FROM
-            FIRST_HALF
-        ORDER BY
-            TOTAL_ORDER DESC,
-            SHIPMENT_ID ASC
-    ) T1
+# SELECT
+#     *
+# FROM
+#     (
+#         SELECT
+#             FLAVOR
+#         FROM
+#             FIRST_HALF
+#         ORDER BY
+#             TOTAL_ORDER DESC,
+#             SHIPMENT_ID ASC
+#     ) T1
 
-    
+SELECT
+    FLAVOR
+FROM
+    FIRST_HALF
+ORDER BY
+    TOTAL_ORDER DESC,
+    SHIPMENT_ID ASC;
